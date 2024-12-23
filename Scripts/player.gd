2b614e7d8a659437f58a082eaae105c13b926a0c
sockarockee <taylorg8900@gmail.com>
2024-12-22 21:12:22 -0700

    Updated player code
    player has acceleration, coyote time, jump cutting, and fixed animated sprite 2d relying on a marker node for flipping horizontally. Also shotgun looks pixelated now for some reason

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,57 +1,73 @@
 extends CharacterBody2D
 
-
-
-@onready var coyote_timer: Timer = $CoyoteTimer
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var marker_2d: Marker2D = $Marker2D
-@onready var animated_sprite: AnimatedSprite2D = $Marker2D/AnimatedSprite2D
+@onready var jump_sound = $JumpSound
+@onready var coyote_timer = $CoyoteTimer
+
+const GRAVITY = 340
+const MAX_HORIZONTAL_SPEED = 67
+const HORIZONTAL_ACCELERATION = 550
+const JUMP_SPEED = 160
+const JUMP_TERMINATION_MULTIPLIER = 4
 
 
-const SPEED = 75.0
-const GRAVITY = 600
-const JUMP_HEIGHT = 200
+func _physics_process(delta):
+	# get a Vector2i representing the inputs (up, right, left)
+	var input_vector = get_input_vector()
+	
+	velocity.x += input_vector.x * HORIZONTAL_ACCELERATION * delta
+	
+	# slows down the player with acceleration, if not holding left or right
+	if input_vector.x == 0:
+		velocity.x = lerp(0.0, velocity.x, pow(2, -30 * delta))
+	
+	# clamp is used to keep velocity within a certain range
+	velocity.x = clamp(velocity.x, -MAX_HORIZONTAL_SPEED, MAX_HORIZONTAL_SPEED)
+	
+	# can jump if either on floor or coyote time hasnt ran out
+	if input_vector.y < 0 && (is_on_floor() || !coyote_timer.is_stopped()):  
+		velocity.y = input_vector.y * JUMP_SPEED
+		
 
+	if velocity.y < 0 && !Input.is_action_pressed("jump"):
+		velocity.y += GRAVITY * JUMP_TERMINATION_MULTIPLIER * delta
+		#velocity.y = 0
+	else:
+		velocity.y += GRAVITY * delta
+		
+	var was_on_floor = is_on_floor()
+	
+	set_velocity(velocity)
+	set_up_direction(Vector2.UP)
+	move_and_slide()
+	velocity = velocity
+	
+	if was_on_floor && !is_on_floor():
+		coyote_timer.start()
 
-func _physics_process(delta: float) -> void:
-	jump_stuff(delta)
+	update_animation(input_vector)
+	
+func get_input_vector():
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left") #basically makes it compatible with controller, dont need but since im stealing it ill keep it
+	input_vector.y = -1 if Input.is_action_just_pressed("jump") else 0
+	return input_vector
 
-	# Get the input direction and handle the movement/deceleration.
+func update_animation(input_vector):
+	'''
 	var direction := Input.get_axis("move_left", "move_right")
 	if direction > 0:
 		marker_2d.scale.x =  1
 	elif direction < 0:
 		marker_2d.scale.x = -1
-	
-	# play animations
-	if is_on_floor():
-		if direction == 0:
-			animated_sprite.play("idle")
-		else:
-			animated_sprite.play("run")
+	'''
+	#if !is_on_floor():
+		#animated_sprite.play("jump")
+	if input_vector.x != 0:
+		animated_sprite.play("run")
 	else:
-		# add jump animation later
-		pass
-	# apply movement
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		animated_sprite.play("idle")
 	
-	# coyote time logic
-	var was_on_floor = is_on_floor()
-	
-	
-	
-	move_and_slide()
-
-func jump_stuff(delta):
-	# logic for jumping, and jump cutting if in the air
-	var jump = Input.is_action_just_pressed("jump")
-	if is_on_floor() and jump:
-		velocity.y = -1 * JUMP_HEIGHT
-	if not is_on_floor():
-		velocity.y += delta * GRAVITY
-	if not is_on_floor() and Input.is_action_just_released("jump") and velocity.y < 0: # jump cutting
-		velocity.y = 0
-	
-	
+	if input_vector.x != 0:
+		animated_sprite.flip_h = true if input_vector.x < 0 else false
