01ddffd2ef99ee5c61c1e5cdb2e0679c0a01e147
sockarockee <taylorg8900@gmail.com>
2025-02-09 19:31:02 -0700

    test

--- Scripts/emerald.gd
+++ Scripts/emerald.gd
@@ -1,5 +1,5 @@
 extends Area2D
-
+ 
 @onready var game_manager: Node = %GameManager
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 @onready var sprite_2d: Sprite2D = $Sprite2D
